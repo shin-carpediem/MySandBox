dacbae6f4b70fa6d12686d7ef1c335622b8042cb
青木振一郎 <shinichiroaoki@aokishinichirounonotobukkukonpyuta.local>
2025-06-15 20:58:04 +0900

    プロトコルの明示的準拠は不要

--- MySandBox/Airticles/SwiftUIArray/Models/MyDataItem.swift
+++ MySandBox/Airticles/SwiftUIArray/Models/MyDataItem.swift
@@ -3,8 +3,4 @@ import Foundation
 struct MyDataItem: Identifiable, Equatable {
     let id = UUID() // ユニークなID
     var name: String
-    // MyDataItemの変更を検知するためにEquatableにも準拠
-    static func == (lhs: MyDataItem, rhs: MyDataItem) -> Bool {
-        lhs.id == rhs.id && lhs.name == rhs.name
-    }
 }
